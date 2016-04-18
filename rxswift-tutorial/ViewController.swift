94ebbe67ce0904047d38fe80d9878bcb32337dfa
plaredspear <plaredspear@gmail.com>
2016-04-18 22:54:38 +0900

    RxSwift를 이용하여 Text 입력을 Label로 출력함

--- rxswift-tutorial/ViewController.swift
+++ rxswift-tutorial/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
+    
+    func bindSourceToLabel(source: PublishSubject<String?>, label: UILabel) {
+        source
+            .subscribeNext { text in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    label.text = text
+                })
+            }
+            .addDisposableTo(disposeBag)
+    }
+    
+    let viewModel = ViewModel()
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var outputText: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Bindin the UI
+        bindSourceToLabel(viewModel.title, label: outputText)
+        bindSourceToLabel(viewModel.titleLabel, label: outputText)
+        
+        inputText.rx_text.subscribeNext { text in
+            self.viewModel.searchText = text
+        }
+        .addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
--- rxswift-tutorial/ViewModel.swift
+++ rxswift-tutorial/ViewModel.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2016 example. All rights reserved.
 //
 
-import Foundation
+import RxCocoa
+import RxSwift
+
+class ViewModel {
+    let disposeBag = DisposeBag()
+    
+    var title = PublishSubject<String?>()
+    var titleLabel = PublishSubject<String?>()
+    
+    var searchText:String? {
+        didSet {
+            if let text = searchText {
+                if text.characters.count > 0 {
+                    titleLabel.on(.Next(text))
+                } else {
+                    titleLabel.on(.Next("Title"))
+                }
+            }
+        }
+    }
+}
